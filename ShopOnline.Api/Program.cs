5d98097ffa251f0da7f5992a297dcc878f0c4020
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-11-18 01:58:28 +0200

    Add project files.

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
+
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors(policy =>
+                policy.WithOrigins("https://localhost:7027", "https://localhost:7027")
+                    .AllowAnyMethod()
+                    .WithHeaders(HeaderNames.ContentType)
+                );
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
